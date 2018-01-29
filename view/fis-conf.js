203c38bfca1ba43c75e1569206bf9e8a9d649a56
bozhao <13207623002#163.com>
2018-01-29 23:10:07 +0800

    首页实现

--- view/fis-conf.js
+++ view/fis-conf.js
@@ -6,7 +6,7 @@
  * 安装less插件 : sudo npm install -g fis-parser-less-2.x
  * 安装sass插件 : sudo npm install fis3-parser-node-sass -g
  * 安装ES6转ES5插件    npm i fis-parser-babel-5.x -g
- * 开发命令     : fis3 server start //开启fis3 本地server
+ * 开发命令     :  fis3 server start //开启fis3 本地server
  *                fis3 server open  //打开fis3 本地server文件夹路径
  *                fis3 server clean //清空本地server文件夹路径内容
  *                fis3 release debug -wL //将当前路径代码发布到本地调试服务器并且监听文件修改自动刷新浏览器
